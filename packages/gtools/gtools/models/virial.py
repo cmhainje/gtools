5e35cbabb1d05063dfc73a8fd481515c881066c0
Connor Hainje <cmhainje@gmail.com>
2022-01-30 17:43:05 -0500

    Change default overdensity to be computed from cosmological params

--- packages/gtools/gtools/models/virial.py
+++ packages/gtools/gtools/models/virial.py
@@ -4,8 +4,8 @@ import astropy.units as u
 import astropy.constants as const
 
 
-class Virial():
-    def __init__(self, cosmology=cosmo.Planck13, redshift=0, overdensity=200):
+class Virial:
+    def __init__(self, cosmology=cosmo.Planck13, redshift=0, overdensity=None):
         """A class for handling computations with Virial quantities.
 
         Parameters
@@ -18,13 +18,21 @@ class Virial():
             The redshift at which to compute the Hubble parameter in the given
             cosmology. Defaults to 0.
         overdensity : int, optional
-            The overdensity defining the Virial object. Defaults to 200.
+            The overdensity defining the Virial object. Defaults to None, in
+            which case it is computed using the Bryan & Norman (1998)
+            prescription from the cosmology and redshift.
         """
         self.cosmology = cosmology
         self.redshift = redshift
-        self.hubble = self.cosmology.H(redshift)
-        self.critical_density = (3 * self.hubble**2) / (8 * np.pi * const.G)
-        self.overdensity = overdensity
+        self.hubble = cosmology.H(redshift)
+        # self.critical_density = (3 * self.hubble**2) / (8 * np.pi * const.G)
+        self.critical_density = cosmology.critical_density(redshift)
+
+        if overdensity is None:
+            O_L = 1 - cosmology.Om(redshift)
+            self.overdensity = 18 * np.pi ** 2 - 82 * O_L - 39 * O_L ** 2
+        else:
+            self.overdensity = overdensity
 
     @u.quantity_input(R=u.kpc)
     def compute_M_from_R(self, R):
@@ -54,9 +62,11 @@ class Virial():
         ----------
         .. [1] https://arxiv.org/pdf/astro-ph/9508025.pdf
         """
-        return (4 / 3 * np.pi * R**3 * self.critical_density * self.overdensity).to(u.M_sun)
-        
-    @u.quantity_input(V=u.km/u.s)
+        return (4 / 3 * np.pi * R ** 3 * self.critical_density * self.overdensity).to(
+            u.M_sun
+        )
+
+    @u.quantity_input(V=u.km / u.s)
     def compute_M_from_V(self, V):
         """Compute the virial mass given the virial velocity.
 
@@ -83,7 +93,7 @@ class Virial():
         ----------
         .. [1] https://arxiv.org/pdf/astro-ph/9707093.pdf
         """
-        return (V**3 / (10 * const.G * self.hubble)).to(u.M_sun)
+        return (V ** 3 / (10 * const.G * self.hubble)).to(u.M_sun)
 
     @u.quantity_input(M=u.M_sun)
     def compute_R_from_M(self, M):
@@ -113,9 +123,11 @@ class Virial():
         ----------
         .. [1] https://arxiv.org/pdf/astro-ph/9508025.pdf
         """
-        return ((3 * M / (4 * np.pi * self.critical_density * self.overdensity))**(1/3)).to(u.kpc)
+        return (
+            (3 * M / (4 * np.pi * self.critical_density * self.overdensity)) ** (1 / 3)
+        ).to(u.kpc)
 
-    @u.quantity_input(V=u.km/u.s)
+    @u.quantity_input(V=u.km / u.s)
     def compute_R_from_V(self, V):
         """Compute the virial radius given the virial velocity.
 
@@ -200,7 +212,7 @@ class Virial():
         ----------
         .. [1] https://arxiv.org/pdf/astro-ph/9707093.pdf
         """
-        return ((10 * const.G * self.hubble * M)**(1/3)).to(u.km / u.s)
+        return ((10 * const.G * self.hubble * M) ** (1 / 3)).to(u.km / u.s)
 
     @u.quantity_input(M=u.M_sun)
     def compute_others_from_M(self, M):
@@ -210,32 +222,31 @@ class Virial():
     def compute_others_from_R(self, R):
         return self.compute_M_from_R(R), self.compute_V_from_R(R)
 
-    @u.quantity_input(V=u.km/u.s)
+    @u.quantity_input(V=u.km / u.s)
     def compute_others_from_V(self, V):
         return self.compute_M_from_V(V), self.compute_R_from_V(V)
 
     def read_and_compute_others(self, M_vir=None, r_vir=None, v_vir=None, **kwargs):
         # there must be at least one virial parameter
         if not (M_vir or r_vir or v_vir):
-            raise ValueError('One virial parameter must be given.')
+            raise ValueError("One virial parameter must be given.")
 
-        # if only one is specified, use virial relationships to compute others 
+        # if only one is specified, use virial relationships to compute others
         if not (r_vir or v_vir):
             r_vir, v_vir = self.compute_others_from_M(M_vir)
         elif not (M_vir or v_vir):
             M_vir, v_vir = self.compute_others_from_R(r_vir)
         elif not (M_vir or r_vir):
             M_vir, r_vir = self.compute_others_from_R(v_vir)
-        
+
         # if two are specified, use v = sqrt(G M / r)
         elif not (v_vir):
             v_vir = np.sqrt(const.G * M_vir / r_vir)
         elif not (r_vir):
-            r_vir = const.G * M_vir / v_vir**2
+            r_vir = const.G * M_vir / v_vir ** 2
         elif not (M_vir):
-            M_vir = r_vir * v_vir**2 / const.G
+            M_vir = r_vir * v_vir ** 2 / const.G
 
         # if all three are specified, do nothing
 
         return M_vir, r_vir, v_vir
-
